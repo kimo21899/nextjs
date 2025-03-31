aff9a6861d15870aa2b166f8dcc3411997dc808c
kimo21899 <lkm24@nave.rcom>
2025-03-31 15:20:28 +0900

    git clone test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Next.Js</h1>
+    <>
+      <h1>Next.Js</h1>
+      <Image 
+        src="next.svg" 
+        width={600} height={400} alt="welcome.." />
+    </>
   );
 }
